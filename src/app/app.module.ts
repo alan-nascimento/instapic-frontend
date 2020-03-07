9dfa76ec1bf2565cdeb4913a72f0114965638efc
Alan Nascimento <alan.silva.n@hotmail.com>
2020-03-07 16:14:29 -0300

    feat: Add Material module and Photo component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MatSliderModule } from '@angular/material/slider';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,12 +8,16 @@ import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import {PhotoComponent} from './presentation/view/shared/components/photo/photo.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PhotoComponent
   ],
   imports: [
     BrowserModule,
+    MatSliderModule,
     AppRoutingModule,
     ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
     BrowserAnimationsModule
---
+++ src/app/presentation/view/shared/components/photo/photo.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-photo',
+  templateUrl: 'photo.component.html',
+  styleUrls: ['./photo.component.scss'],
+})
+export class PhotoComponent {
+  description = 'Madara';
+  url = 'https://i0.wp.com/www.fatosdesconhecidos.com.br/wp-content/uploads/2017/10/maxresdefault-19.jpg?resize=900,420';
+}
