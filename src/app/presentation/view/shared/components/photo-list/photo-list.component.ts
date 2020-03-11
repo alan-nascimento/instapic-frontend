102e08e26cf05a09e7cad8fdd244735f4b101624
Alan Nascimento <alan.silva.n@hotmail.com>
2020-03-11 02:15:12 -0300

    feat: Add load more data button

--- src/app/presentation/view/shared/components/photo-list/photo-list.component.ts
+++ src/app/presentation/view/shared/components/photo-list/photo-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 import { IPhoto } from '../photo/photo.interface';
+import { PhotoService } from '../photo/photo.service';
 
 @Component({
   selector: 'app-photo-list',
@@ -11,10 +12,27 @@ import { IPhoto } from '../photo/photo.interface';
 export class PhotoListComponent implements OnInit {
 
   photos: IPhoto[] = [];
+  hasMore = true;
+  username = '';
+  currentPage = 1;
 
-  constructor(private activatedRoute: ActivatedRoute) { }
+  constructor(private activatedRoute: ActivatedRoute, private photoService: PhotoService) { }
 
   public ngOnInit(): void {
+    this.username = this.activatedRoute.snapshot.params.username;
     this.photos = this.activatedRoute.snapshot.data.photos;
+  }
+
+  public load() {
+    this.photoService
+      .getPaginatedUserPhotos(this.username, ++this.currentPage)
+      .subscribe(photos => {
+        if (!photos.length) {
+         this.hasMore = false;
+         return;
+        }
+
+        this.photos = [...this.photos, ...photos];
+      });
   }
 }
--- src/app/presentation/view/shared/shared.module.ts
+++ src/app/presentation/view/shared/shared.module.ts
@@ -7,12 +7,14 @@ import { AppMaterialModule } from 'src/app/app-material.module';
 import { PhotoComponent } from './components/photo/photo.component';
 import { PhotoListComponent } from './components/photo-list/photo-list.component';
 import { PhotoFormComponent } from './components/photo-form/photo-form.component';
+import { LoadButtonComponent } from './components/photo-list/load-button/load-button.component';
 
 @NgModule({
   declarations: [
     PhotoComponent,
     PhotoListComponent,
     PhotoFormComponent,
+    LoadButtonComponent,
   ],
   imports: [
     CommonModule,
